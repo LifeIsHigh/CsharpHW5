e83791e8a84c50765bcc15254a30d1a2ac932696
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-01-19 10:59:39 +0300

    Исправлена опечатка в сообщении

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введитет 3-хзначное значение: ");
+﻿Console.Write("Введите 3-хзначное значение: ");
 int x = Convert.ToInt32(Console.ReadLine());
 int z = 0;
 Console.WriteLine(z = (x % 100) / 10);
